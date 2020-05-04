3530faf0cfcb600bdf769b1bd3d758f383bf5908
Aktolkyn Bektemir <190107077@stu.sdu.edu.kz>
2020-05-04 17:32:21 +0500

    Getter методты пайдаландық
    Getter methods  пайдаланудың арқасында subclass-та тұрған data fields-ті
    Fruit классында қолдана алдық.Приведение типов жасамай-ақ.
    Есептеуді қасқарттық, яғни жалпы сумманы есептеген кезде бірден салыстырып
    кодты барынша қысқа қылып өзгерттік.
    
    P.S Бұл есептегі main метод өзгертілмейді

--- src/FruitJava.java
+++ src/FruitJava.java
@@ -2,98 +2,135 @@ import java.util.Scanner;
 
 abstract class Fruit{
 
-//    public int sum = 0;
+    public abstract String toString();      //осы методты пайдалану үшін абстракт қыламыз
 
-    public String toString(){
-        return "She bought most ";
-    }
+    public abstract int getWeight();        //басқа сабкласстардан салмақты алу үшін
+
+    public abstract int getPrice();         //басқа сабкласстардан бағасын алу үшін
 
     public static void iAmBrainCalculator(Fruit[] fruits){
 
-        int a[] = new int[3];
-        for(int i = 0; i < fruits.length; i++){
-            if(fruits[i] instanceof Apple){
-                a[0] += ((Apple)fruits[i]).weight * ((Apple)fruits[i]).price/1000;
-            }
-            if(fruits[i] instanceof Pear){
-                a[1] += ((Pear)fruits[i]).weight * ((Pear)fruits[i]).price/1000;
-            }
-            if(fruits[i] instanceof Banana){
-                a[2] += ((Banana)fruits[i]).weight * ((Banana)fruits[i]).price/1000;
-            }
-        }
-        int max = 0;
+        /*  Fruit массивін қабылдап алады
+         *  сосын типтері бірдей элементердің бағасын есептеп жинақтайды
+         *  жинақталғандардың ішінен ең үлкенін анықтайды
+         *  сосын суммасы шығарады
+         *  сол типтің toString методын шақырады
+         */
+        float a[] = new float[3];
+
         int index = 0;
-        int sum = 0;
-        for(int i = 0; i < a.length; i++){
-            if(a[i] > max){
-                max = a[i];
-                index = i;
+        float max = 0;
+        float sum = 0;
+
+        Fruit f;
+        for (int i = 0; i < fruits.length; i++) {
+
+          f = fruits[i];
+
+          if ( f instanceof Apple ) {
+
+            a[0] += f.getWeight() * f.getPrice() / 1000F;   // дұрыс есептеу үшін айнымалылырды float типінде алдық
+            if ( max < a[0] ) {
+              max = a[0];
+              index = i;
+              }
+          }
+
+          if ( f instanceof Pear) {
+            a[1] += f.getWeight() * f.getPrice() / 1000F;
+            if(max < a[1]) {
+              max = a[1];
+              index = i;
             }
-            sum += a[i];
-        }
-        if(index == 0){
-            System.out.println(this.toString());
-        }
-        if(index == 1){
-            System.out.println(this.toString("Pear!"));
-        }
-        if(index == 2){
-            System.out.println(this.toString("Banana!"));
+          }
+
+          if ( f instanceof Banana) {
+            a[2] += f.getWeight() * f.getPrice() / 1000F;
+            if(max < a[2]) {
+              max = a[2];
+              index = i;
+            }
+          }
         }
 
+        sum = a[0] + a[1] + a[2];
+
+        System.out.println(fruits[index]);
+        System.out.println("Apple " + a[0]); // Тексеру үшін
+        System.out.println("Pear " + a[1]);
+        System.out.println("Banana " + a[2]);
+
+        System.out.println("Total Price: " + Math.round(sum)); // шыққан бағаны бүтін санға айналдыру үшін
         System.out.println("Total Price: " + sum);
     }
 
 }
 
 class Apple extends Fruit{
-    public int price = 400;
+
+    public int price = 423;
     public int weight;
-
-
-
-//    public Apple(){}
 
     public Apple(int weight){
         this.weight = weight;
     }
 
-//    @Override
-//    public String toString(){
-//        return super.toString() + "Apple!";
-//    }
+    @Override
+    public String toString() {
+        return "She bought most Apple";
+    }
+    @Override
+    public int getWeight(){
+      return weight;
+    }
+    public int getPrice(){
+      return price;
+    }
 }
 
 class Pear extends Fruit{
-    public int price = 600;
-    public int weight;
 
-//    public Pear(){}
+    public int price = 612;
+    public int weight;
 
     public Pear(int weight){
         this.weight = weight;
+
     }
 
-//    @Override
-//    public String toString(){
-//        return super.toString() + "Apple!";
-//    }
+    @Override
+    public String toString() {
+        return "She bought most Pear";
+    }
+
+    @Override
+    public int getWeight(){
+      return weight;
+    }
+    public int getPrice(){
+      return price;
+    }
 }
 class Banana extends Fruit{
-    public int price = 400;
+    public int price = 350;
     public int weight;
-
-//    public Banana(){}
 
     public Banana(int weight){
         this.weight = weight;
     }
 
-//    @Override
-//    public String toString(){
-//        return super.toString() + "Apple!";
-//    }
+    @Override
+    public String toString() {
+        return "She bought most Banana";
+    }
+
+    @Override
+    public int getWeight(){
+      return weight;
+    }
+    public int getPrice(){
+      return price;
+    }
 }
 
 public class FruitJava {
